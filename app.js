21b380faeb35b15ea3f275d61b9690425146b766
Saurabh <saurabh.saini@te.com>
2022-09-02 11:17:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require('cors');
+const io = require('socket.io');
+
+const app = express();
+app.enable('trust proxy');
+
+
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use(express.static(__dirname));
+
+app.use((req, res, next) => {
+	console.log("query:", req.query);
+  console.log("body:", req.body);
+	next();
+},
+cors()
+);
+
+app.get("/", (req, res) => {
+    res.sendStatus(200).json({
+        result:"Done"
+    })
+});
+
+app.get("/events", (req, res) => {
+    res.sendStatus(200).json({
+        result:"Done"
+    })
+});
+
+app.listen(3000, () => {
+    console.log("server is runing on 3000 port")
+})
+
